52b60e8c6828e00088f908ca083a9a4df8e2e364
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-01-15 02:08:53 +0600

    User and profile Orders Update

--- backend/routes/CutomerRoutes/users.js
+++ backend/routes/CutomerRoutes/users.js
@@ -1,9 +1,12 @@
 const express=require('express')
 const router=express.Router()
-const {getUser, getOrderById} = require('../../controllers/Customer/userController')
+const {getUser, getOrderById, updateUser, deleteUser} = require('../../controllers/Customer/userController')
 //GET USER
 router.get("/Customer/:id", getUser);
 //get Orders of User
 router.get("/Customer/getOrders/:id", getOrderById);
-
+//Update User
+router.put("/Customer/:id", updateUser);
+//Delete User
+router.delete("/Customer/:id", deleteUser);
 module.exports=router
